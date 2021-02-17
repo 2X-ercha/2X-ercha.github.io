f2008a7f5b2180bca776c4db6a83b88347173d8a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-17 13:46:40 +0000

    Deploy 2X-ercha/2X-ercha.github.io to github.com/2X-ercha/2X-ercha.github.io.git:gh-pages

--- friendcircle/request.js
+++ friendcircle/request.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/css/APlayer.min.css"><script src="/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/js/Meting.min.js"></script>requests_url = 'https://hexo-circle-of-friends-api.2x-ercha.vercel.app/api';
+requests_url = 'https://hexo-circle-of-friends-api.2x-ercha.vercel.app/api';
 const friend_link_circle = new Vue({
     el: '#friend_link_circle',
     data: {
