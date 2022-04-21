0328c28f27b84b8955883daad89298ff8a41a9f7
noionion <noionion@outlook.com>
2022-04-21 12:39:06 +0800

    main.js updated

--- themes/butterfly/source/js/main.js
+++ themes/butterfly/source/js/main.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', function () {
   let menusWidth = $menusEle && $menusEle.offsetWidth
   const $searchEle = document.querySelector('#search-button')
   let searchWidth = $searchEle && $searchEle.offsetWidth
+  //begin 屏幕长度缩减修复（
+  const $darkmodeBtEle = document.querySelector('#darkmodeBt')
+  let darkmodeBtWidth = $darkmodeBtEle && $darkmodeBtEle.offsetWidth
+  //end
 
   const adjustMenu = (change = false) => {
     if (change) {
@@ -14,8 +18,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     const $nav = document.getElementById('nav')
     let t
-    if (window.innerWidth < 768) t = true
-    else t = blogNameWidth + menusWidth + searchWidth > $nav.offsetWidth - 120
+    if (window.innerWidth <= 900) t = true
+    //begin 屏幕长度缩减修复（
+    else t = blogNameWidth + menusWidth + searchWidth + darkmodeBtWidth > $nav.offsetWidth - 120
+    //end
 
     if (t) {
       $nav.classList.add('hide-menu')
@@ -81,121 +87,140 @@ document.addEventListener('DOMContentLoaded', function () {
  * 只適用於Hexo默認的代碼渲染
  */
   const addHighlightTool = function () {
-    const isHighlightCopy = GLOBAL_CONFIG.highlight.highlightCopy
-    const isHighlightLang = GLOBAL_CONFIG.highlight.highlightLang
+    const highLight = GLOBAL_CONFIG.highlight
+    if (!highLight) return
+
+    const isHighlightCopy = highLight.highlightCopy
+    const isHighlightLang = highLight.highlightLang
     const isHighlightShrink = GLOBAL_CONFIG_SITE.isHighlightShrink
+    const highlightHeightLimit = highLight.highlightHeightLimit
     const isShowTool = isHighlightCopy || isHighlightLang || isHighlightShrink !== undefined
-    const $figureHighlight = GLOBAL_CONFIG.highlight.plugin === 'highlighjs' ? document.querySelectorAll('figure.highlight') : document.querySelectorAll('pre[class*="language-"]')
+    const $figureHighlight = highLight.plugin === 'highlighjs' ? document.querySelectorAll('figure.highlight') : document.querySelectorAll('pre[class*="language-"]')
 
-    if (isShowTool && $figureHighlight.length) {
-      const isPrismjs = GLOBAL_CONFIG.highlight.plugin === 'prismjs'
+    if (!((isShowTool || highlightHeightLimit) && $figureHighlight.length)) return
 
-      let highlightShrinkEle = ''
-      let highlightCopyEle = ''
-      const highlightShrinkClass = isHighlightShrink === true ? 'closed' : ''
+    const isPrismjs = highLight.plugin === 'prismjs'
 
-      if (isHighlightShrink !== undefined) {
-        highlightShrinkEle = `<i class="fas fa-angle-down expand ${highlightShrinkClass}"></i>`
-      }
+    let highlightShrinkEle = ''
+    let highlightCopyEle = ''
+    const highlightShrinkClass = isHighlightShrink === true ? 'closed' : ''
 
-      if (isHighlightCopy) {
-        highlightCopyEle = '<div class="copy-notice"></div><i class="fas fa-paste copy-button"></i>'
-      }
+    if (isHighlightShrink !== undefined) {
+      highlightShrinkEle = `<i class="fas fa-angle-down expand ${highlightShrinkClass}"></i>`
+    }
 
-      const copy = (text, ctx) => {
-        if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
-          document.execCommand('copy')
-          if (GLOBAL_CONFIG.Snackbar !== undefined) {
-            btf.snackbarShow(GLOBAL_CONFIG.copy.success)
-          } else {
-            const prevEle = ctx.previousElementSibling
-            prevEle.innerText = GLOBAL_CONFIG.copy.success
-            prevEle.style.opacity = 1
-            setTimeout(() => { prevEle.style.opacity = 0 }, 700)
-          }
+    if (isHighlightCopy) {
+      highlightCopyEle = '<div class="copy-notice"></div><i class="fas fa-paste copy-button"></i>'
+    }
+
+    const copy = (text, ctx) => {
+      if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+        document.execCommand('copy')
+        if (GLOBAL_CONFIG.Snackbar !== undefined) {
+          btf.snackbarShow(GLOBAL_CONFIG.copy.success)
         } else {
-          if (GLOBAL_CONFIG.Snackbar !== undefined) {
-            btf.snackbarShow(GLOBAL_CONFIG.copy.noSupport)
-          } else {
-            ctx.previousElementSibling.innerText = GLOBAL_CONFIG.copy.noSupport
-          }
-        }
-      }
-
-      // click events
-      const highlightCopyFn = (ele) => {
-        const $buttonParent = ele.parentNode
-        $buttonParent.classList.add('copy-true')
-        const selection = window.getSelection()
-        const range = document.createRange()
-        if (isPrismjs) range.selectNodeContents($buttonParent.querySelectorAll('pre code')[0])
-        else range.selectNodeContents($buttonParent.querySelectorAll('table .code pre')[0])
-        selection.removeAllRanges()
-        selection.addRange(range)
-        const text = selection.toString()
-        copy(text, ele.lastChild)
-        selection.removeAllRanges()
-        $buttonParent.classList.remove('copy-true')
-      }
-
-      const highlightShrinkFn = (ele) => {
-        const $nextEle = [...ele.parentNode.children].slice(1)
-        ele.firstChild.classList.toggle('closed')
-        if (btf.isHidden($nextEle[0])) {
-          $nextEle.forEach(e => { e.style.display = 'block' })
-        } else {
-          $nextEle.forEach(e => { e.style.display = 'none' })
-        }
-      }
-
-      const highlightToolsFn = function (e) {
-        const $target = e.target.classList
-        if ($target.contains('expand')) highlightShrinkFn(this)
-        else if ($target.contains('copy-button')) highlightCopyFn(this)
-      }
-
-      const createEle = () => {
-        const newEle = document.createElement('div')
-        newEle.className = `highlight-tools ${highlightShrinkClass}`
-        newEle.addEventListener('click', highlightToolsFn)
-        return newEle
-      }
-
-      if (isHighlightLang) {
-        if (isPrismjs) {
-          $figureHighlight.forEach(function (item) {
-            const langName = item.getAttribute('data-language') !== undefined ? item.getAttribute('data-language') : 'Code'
-            const highlightLangEle = `<div class="code-lang">${langName}</div>`
-            btf.wrap(item, 'figure', '', 'highlight')
-            const newEle = createEle()
-            newEle.innerHTML = highlightShrinkEle + highlightLangEle + highlightCopyEle
-            item.parentNode.insertBefore(newEle, item)
-          })
-        } else {
-          $figureHighlight.forEach(function (item) {
-            let langName = item.getAttribute('class').split(' ')[1]
-            if (langName === 'plain' || langName === undefined) langName = 'Code'
-            const highlightLangEle = `<div class="code-lang">${langName}</div>`
-            const newEle = createEle()
-            newEle.innerHTML = highlightShrinkEle + highlightLangEle + highlightCopyEle
-            item.insertBefore(newEle, item.firstChild)
-          })
+          const prevEle = ctx.previousElementSibling
+          prevEle.innerText = GLOBAL_CONFIG.copy.success
+          prevEle.style.opacity = 1
+          setTimeout(() => { prevEle.style.opacity = 0 }, 700)
         }
       } else {
-        if (isPrismjs) {
-          $figureHighlight.forEach(function (item) {
-            btf.wrap(item, 'figure', '', 'highlight')
-            const newEle = createEle()
-            newEle.innerHTML = highlightShrinkEle + highlightCopyEle
-            item.parentNode.insertBefore(newEle, item)
-          })
+        if (GLOBAL_CONFIG.Snackbar !== undefined) {
+          btf.snackbarShow(GLOBAL_CONFIG.copy.noSupport)
         } else {
-          $figureHighlight.forEach(function (item) {
-            const newEle = createEle()
-            newEle.innerHTML = highlightShrinkEle + highlightCopyEle
-            item.insertBefore(newEle, item.firstChild)
-          })
+          ctx.previousElementSibling.innerText = GLOBAL_CONFIG.copy.noSupport
         }
+      }
+    }
+
+    // click events
+    const highlightCopyFn = (ele) => {
+      const $buttonParent = ele.parentNode
+      $buttonParent.classList.add('copy-true')
+      const selection = window.getSelection()
+      const range = document.createRange()
+      if (isPrismjs) range.selectNodeContents($buttonParent.querySelectorAll('pre code')[0])
+      else range.selectNodeContents($buttonParent.querySelectorAll('table .code pre')[0])
+      selection.removeAllRanges()
+      selection.addRange(range)
+      const text = selection.toString()
+      copy(text, ele.lastChild)
+      selection.removeAllRanges()
+      $buttonParent.classList.remove('copy-true')
+    }
+
+    const highlightShrinkFn = (ele) => {
+      const $nextEle = [...ele.parentNode.children].slice(1)
+      ele.firstChild.classList.toggle('closed')
+      if (btf.isHidden($nextEle[$nextEle.length - 1])) {
+        $nextEle.forEach(e => { e.style.display = 'block' })
+      } else {
+        $nextEle.forEach(e => { e.style.display = 'none' })
+      }
+    }
+
+    const highlightToolsFn = function (e) {
+      const $target = e.target.classList
+      if ($target.contains('expand')) highlightShrinkFn(this)
+      else if ($target.contains('copy-button')) highlightCopyFn(this)
+    }
+
+    const expandCode = function () {
+      this.classList.toggle('expand-done')
+    }
+
+    function createEle (lang, item, service) {
+      const fragment = document.createDocumentFragment()
+
+      if (isShowTool) {
+        const hlTools = document.createElement('div')
+        hlTools.className = `highlight-tools ${highlightShrinkClass}`
+        hlTools.innerHTML = highlightShrinkEle + lang + highlightCopyEle
+        hlTools.addEventListener('click', highlightToolsFn)
+        fragment.appendChild(hlTools)
+      }
+
+      if (highlightHeightLimit && item.offsetHeight > highlightHeightLimit + 30) {
+        const ele = document.createElement('div')
+        ele.className = 'code-expand-btn'
+        ele.innerHTML = '<i class="fas fa-angle-double-down"></i>'
+        ele.addEventListener('click', expandCode)
+        fragment.appendChild(ele)
+      }
+
+      if (service === 'hl') {
+        item.insertBefore(fragment, item.firstChild)
+      } else {
+        item.parentNode.insertBefore(fragment, item)
+      }
+    }
+
+    if (isHighlightLang) {
+      if (isPrismjs) {
+        $figureHighlight.forEach(function (item) {
+          const langName = item.getAttribute('data-language') ? item.getAttribute('data-language') : 'Code'
+          const highlightLangEle = `<div class="code-lang">${langName}</div>`
+          btf.wrap(item, 'figure', { class: 'highlight' })
+          createEle(highlightLangEle, item)
+        })
+      } else {
+        $figureHighlight.forEach(function (item) {
+          let langName = item.getAttribute('class').split(' ')[1]
+          if (langName === 'plain' || langName === undefined) langName = 'Code'
+          const highlightLangEle = `<div class="code-lang">${langName}</div>`
+          createEle(highlightLangEle, item, 'hl')
+        })
+      }
+    } else {
+      if (isPrismjs) {
+        $figureHighlight.forEach(function (item) {
+          btf.wrap(item, 'figure', { class: 'highlight' })
+          createEle('', item)
+        })
+      } else {
+        $figureHighlight.forEach(function (item) {
+          createEle('', item, 'hl')
+        })
       }
     }
   }
@@ -275,7 +300,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   const addMediumZoom = () => {
-    const zoom = mediumZoom(document.querySelectorAll('#article-container :not(a)>img'))
+    const zoom = mediumZoom(document.querySelectorAll('#article-container :not(a):not(.flink-item-icon) > img'))
     zoom.on('open', e => {
       const photoBg = document.documentElement.getAttribute('data-theme') === 'dark' ? '#121212' : '#fff'
       zoom.update({
@@ -286,7 +311,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const jqLoadAndRun = () => {
     const $fancyboxEle = GLOBAL_CONFIG.lightbox === 'fancybox'
-      ? document.querySelectorAll('#article-container :not(a):not(.gallery-group) > img, #article-container > img')
+      ? document.querySelectorAll('#article-container :not(a):not(.gallery-group):not(.flink-item-icon) > img, #article-container > img')
       : []
     const fbLengthNoZero = $fancyboxEle.length > 0
     const $jgEle = document.querySelectorAll('#article-container .justified-gallery')
@@ -313,50 +338,55 @@ document.addEventListener('DOMContentLoaded', function () {
       return
     }
 
-    let initTop = 0
-    let isChatShow = true
-    const $header = document.getElementById('page-header')
-    const isChatBtnHide = typeof chatBtnHide === 'function'
-    const isChatBtnShow = typeof chatBtnShow === 'function'
-    window.addEventListener('scroll', btf.throttle(function (e) {
-      const currentTop = window.scrollY || document.documentElement.scrollTop
-      const isDown = scrollDirection(currentTop)
-      if (currentTop > 56) {
-        if (isDown) {
-          if ($header.classList.contains('nav-visible')) $header.classList.remove('nav-visible')
-          if (isChatBtnShow && isChatShow === true) {
-            chatBtnHide()
-            isChatShow = false
-          }
-        } else {
-          if (!$header.classList.contains('nav-visible')) $header.classList.add('nav-visible')
-          if (isChatBtnHide && isChatShow === false) {
-            chatBtnShow()
-            isChatShow = true
-          }
-        }
-        $header.classList.add('nav-fixed')
-        if (window.getComputedStyle($rightside).getPropertyValue('opacity') === '0') {
-          $rightside.style.cssText = 'opacity: 1; transform: translateX(-38px)'
-        }
-      } else {
-        if (currentTop === 0) {
-          $header.classList.remove('nav-fixed', 'nav-visible')
-        }
-        $rightside.style.cssText = "opacity: ''; transform: ''"
-      }
-
-      if (document.body.scrollHeight <= innerHeight) {
-        $rightside.style.cssText = 'opacity: 1; transform: translateX(-38px)'
-      }
-    }, 200))
-
     // find the scroll direction
     function scrollDirection (currentTop) {
       const result = currentTop > initTop // true is down & false is up
       initTop = currentTop
       return result
     }
+
+    let initTop = 0
+    let isChatShow = true
+    const $header = document.getElementById('page-header')
+    const isChatBtnHide = typeof chatBtnHide === 'function'
+    const isChatBtnShow = typeof chatBtnShow === 'function'
+
+    window.scrollCollect = () => {
+      return btf.throttle(function (e) {
+        const currentTop = window.scrollY || document.documentElement.scrollTop
+        const isDown = scrollDirection(currentTop)
+        if (currentTop > 56) {
+          if (isDown) {
+            if ($header.classList.contains('nav-visible')) $header.classList.remove('nav-visible')
+            if (isChatBtnShow && isChatShow === true) {
+              chatBtnHide()
+              isChatShow = false
+            }
+          } else {
+            if (!$header.classList.contains('nav-visible')) $header.classList.add('nav-visible')
+            if (isChatBtnHide && isChatShow === false) {
+              chatBtnShow()
+              isChatShow = true
+            }
+          }
+          $header.classList.add('nav-fixed')
+          if (window.getComputedStyle($rightside).getPropertyValue('opacity') === '0') {
+            $rightside.style.cssText = 'opacity: 1; transform: translateX(-38px)'
+          }
+        } else {
+          if (currentTop === 0) {
+            $header.classList.remove('nav-fixed', 'nav-visible')
+          }
+          $rightside.style.cssText = "opacity: ''; transform: ''"
+        }
+
+        if (document.body.scrollHeight <= innerHeight) {
+          $rightside.style.cssText = 'opacity: 1; transform: translateX(-38px)'
+        }
+      }, 200)()
+    }
+
+    window.addEventListener('scroll', scrollCollect)
   }
 
   /**
@@ -369,11 +399,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const $article = document.getElementById('article-container')
 
     // main of scroll
-    window.addEventListener('scroll', btf.throttle(function (e) {
-      const currentTop = window.scrollY || document.documentElement.scrollTop
-      scrollPercent(currentTop)
-      findHeadPosition(currentTop)
-    }, 100))
+    window.tocScrollFn = function () {
+      return btf.throttle(function () {
+        const currentTop = window.scrollY || document.documentElement.scrollTop
+        scrollPercent(currentTop)
+        findHeadPosition(currentTop)
+      }, 100)()
+    }
+    window.addEventListener('scroll', tocScrollFn)
 
     const scrollPercent = function (currentTop) {
       const docHeight = $article.clientHeight
@@ -391,7 +424,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const updateAnchor = function (anchor) {
       if (window.history.replaceState && anchor !== window.location.hash) {
         if (!anchor) anchor = location.pathname
-        window.history.replaceState({}, '', anchor)
+        const title = GLOBAL_CONFIG_SITE.title
+        window.history.replaceState({
+          url: location.href,
+          title: title
+        }, title, anchor)
       }
     }
 
@@ -661,7 +698,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const $table = document.querySelectorAll('#article-container :not(.highlight) > table, #article-container > table')
     if ($table.length) {
       $table.forEach(item => {
-        btf.wrap(item, 'div', '', 'table-wrap')
+        btf.wrap(item, 'div', { class: 'table-wrap' })
       })
     }
   }
@@ -819,7 +856,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     sidebarFn()
     GLOBAL_CONFIG_SITE.isHome && scrollDownInIndex()
-    GLOBAL_CONFIG.highlight && addHighlightTool()
+    addHighlightTool()
     GLOBAL_CONFIG.isPhotoFigcaption && addPhotoFigcaption()
     jqLoadAndRun()
     GLOBAL_CONFIG.lightbox === 'mediumZoom' && addMediumZoom()
