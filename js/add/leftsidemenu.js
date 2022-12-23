73588b843b3890ed6e5b0f687191b698ac83d736
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-12-23 15:20:23 +0000

    Deploy 2X-ercha/2X-ercha.github.io to github.com/2X-ercha/2X-ercha.github.io.git:gh-pages

--- js/add/leftsidemenu.js
+++ js/add/leftsidemenu.js
@@ -2,23 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const leftSideMenu = {
         qrcodeHover: () => {
             // 按钮获取
-            const $left_menu = document.getElementById('left_menu')
-            const $leftside_qq = $left_menu.getElementsByClassName('leftside-qq')[0]
-            //const $leftside_qq = $left_menu.getElementById('leftside-qq')
-            // 显示区域获取
-            const $left_display = document.getElementById('left_display')
-            const $candy_qrcode = $left_display.getElementsByClassName('candy_qrcode')[0]
-            
-            $leftside_qq.addEventListener('onmouseover', function () {
-                console.log("111")
-                $candy_qrcode.style.opacity = "1"
-            })
+            const left_menu = document.getElementById('left_menu')
+            if(left_menu){
+                const leftside_qq = left_menu.getElementsByClassName('leftside-qq')[0]
+                // 显示区域获取
+                const left_display = document.getElementById('left_display')
+                const candy_qrcode = left_display.getElementsByClassName('candy_qrcode')[0]
 
-            $leftside_qq.addEventListener('onmouseout', function () {
-                console.log("222")
-                $candy_qrcode.style.opacity = "0"
-            })
-           
+                leftside_qq.addEventListener('mouseover', function () {
+                    candy_qrcode.style.opacity = "1"
+                })
+                leftside_qq.addEventListener('mouseout', function () {
+                    candy_qrcode.style.opacity = "0"
+                })
+            }
         }
     }
 
